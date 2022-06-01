cc1c73e38166716eb44b48c7adca259a86c63c25
Juan Betancourt <juancbetancourt11@gmail.com>
2022-06-01 01:18:34 +0000

    Added random account and user input

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #To-Do-List
 ## Generate a random account for the game_data
-
+import random
+def get_random_account():
+  """Get data from a random account"""
+  return random.choice(data)
 ## Format account data into printable format
-
+def format_data(account):
+  
 ## Ask the user for a guess
+guess = input("Who has more followers A or B? ").lower()
 
 ## Check if user is correct
 ## Get follower count
