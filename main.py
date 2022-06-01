9f52b9d2ebbf83443a7fa6991ae0a14e6f5ab54e
Juan Betancourt <juancbetancourt11@gmail.com>
2022-06-01 00:50:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+## Generate a random account for the game_data
+
+## Format account data into printable format
+
+## Ask the user for a guess
+
+## Check if user is correct
+## Get follower count
+## if statement
+
+## Feedback
+
+## Score keeping
+
+## Make game repeatable
+
+## Make B the next A
+
+## Add Art
+
+## Clear screen between rounds
